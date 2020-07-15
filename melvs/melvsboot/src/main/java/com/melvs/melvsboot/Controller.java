4f62aad468720bd657e4b22334b919b22e54b0ae
melvin-john-banaag <melvin.john.banaag@gmail.com>
2020-07-15 13:37:30 +0800

    Sample GET request

--- melvs/melvsboot/src/main/java/com/melvs/melvsboot/Controller.java
+++ melvs/melvsboot/src/main/java/com/melvs/melvsboot/Controller.java
@@ -4,12 +4,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 //controller rest mapping
 @RestController
-public class GetEnd {
+public class Controller {
 
 //request mapping
     @RequestMapping("/GetEnd")
-    public String getend() {
-        return "This is a value return back";}
-}
+    public List<GetList> getAllList() {
+        return Arrays.asList(
+            new GetList("1","Melvin","This is test"),
+            new GetList("2","John","This is test again"),
+            new GetList("3","Bana-ag","This is another test")
+            );
+        }
+
+    }
