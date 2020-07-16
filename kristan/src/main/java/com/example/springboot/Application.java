03ac5667ce35aa1e8e6fa16912eaa34da75b3439
kristan <kristan.eres@gmail.com>
2020-07-16 15:19:12 +0800

    removed commented code and unsued imports

--- kristan/src/main/java/com/example/springboot/Application.java
+++ kristan/src/main/java/com/example/springboot/Application.java
@@ -4,21 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.example.springboot.model.Student;
 import com.example.springboot.view.Students;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedList;
 
 @SpringBootApplication
 public class Application {
-
-//	@Bean
-//	public ArrayList<Student> initStudentList() {
-//		Students studentInfo = new Students();
-//		return studentInfo.getAllStudents();
-//	}
 
 	@Bean
 	public LinkedList<String> initStudents() {
