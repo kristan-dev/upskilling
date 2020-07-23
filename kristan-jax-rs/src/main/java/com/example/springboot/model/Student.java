b01993030bb98a996668a5bf924d53e669ed130b
Kristan Eres <kristan.sangalang@gmail.com>
2020-07-23 14:07:35 +0800

    implemented NoArgsConstructor and AllArgsConstructor

--- kristan-jax-rs/src/main/java/com/example/springboot/model/Student.java
+++ kristan-jax-rs/src/main/java/com/example/springboot/model/Student.java
@@ -1,19 +1,23 @@
 package com.example.springboot.model;
 
 import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 // A shortcut for @ToString, @EqualsAndHashCode, @Getter on all fields, 
 // and @Setter on all non-final fields, and @RequiredArgsConstructor!
 public class Student {
 
-    private final String name;
-    private final String student_id;
-    private final String program;
+    private String name;
+    private String student_id;
+    private String program;
 
-    public Student(String name, String student_id, String program) {
-        this.name = name;
-        this.student_id = student_id;
-        this.program = program;
-    }
+//    public Student(String name, String student_id, String program) {
+//        this.name = name;
+//        this.student_id = student_id;
+//        this.program = program;
+//    }
 }
