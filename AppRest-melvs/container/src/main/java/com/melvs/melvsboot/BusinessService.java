4cec513ca627cbc2f521a904b29c57da319b10d7
melvin-john-banaag <melvin.john.banaag@gmail.com>
2020-07-26 23:11:14 +0800

    updated with container

---
+++ AppRest-melvs/container/src/main/java/com/melvs/melvsboot/BusinessService.java
@@ -0,0 +1,21 @@
+package com.melvs.melvsboot;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+//service once spring boot started will load to memory
+@Service
+public class BusinessService {
+
+    private List<GetList> list = Arrays.asList(
+            new GetList("1","Melvin","This is test"),
+            new GetList("2","John","This is test again"),
+            new GetList("3","Bana-ag","This is another test")
+            );
+
+    public List<GetList> getAllList () {
+        return list;
+    }
+}
---
+++ AppRest-melvs/container/src/main/java/com/melvs/melvsboot/MelvsbootApplication.java
@@ -0,0 +1,14 @@
+package com.melvs.melvsboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//starting point of spring bootstrap
+@SpringBootApplication
+public class MelvsbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelvsbootApplication.class, args);
+	}
+
+}
