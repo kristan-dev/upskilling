9499b36486d751a7735f8fcb7ac325a88300c068
melvin-john-banaag <melvin.john.banaag@gmail.com>
2020-07-27 20:11:18 +0800

    included list on issue

--- AppRest-melvs/container/src/main/java/com/melvs/melvsboot/model/GetList.java
+++ AppRest-melvs/container/src/main/java/com/melvs/melvsboot/model/GetList.java
@@ -1,6 +1,12 @@
 package com.melvs.melvsboot.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GetList {
 
 //string variable
@@ -8,34 +14,4 @@ public class GetList {
     private String name;
     private String description;
 
-//to initialize
-    public GetList(){
-
-    }
-
-    public GetList(String id,String name,String description){
-        super();
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
-
-    public String getId() { return id; }
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- AppRest-melvs/container/src/main/java/com/melvs/melvsboot/model/GetListBuilder.java
+++ AppRest-melvs/container/src/main/java/com/melvs/melvsboot/model/GetListBuilder.java
@@ -1,4 +1,17 @@
 package com.melvs.melvsboot.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GetListBuilder {
+
+    private String id;
+    private String name;
+    private String description;
 }
